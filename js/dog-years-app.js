e4ff2cf0b2478446b89d4a2a534762bd08b020fd
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-05-03 14:24:10 -0500

    Dog Years App

---
+++ js/dog-years-app.js
@@ -0,0 +1,14 @@
+let myAge = 28;
+// My age
+let earlyYears =2;
+//something about years
+earlyYears = earlyYears * 10.5;
+//times it
+let laterYears = myAge - 2;
+//times it
+laterYears = laterYears * 4;
+// tacos
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Timothy';
+myName.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
